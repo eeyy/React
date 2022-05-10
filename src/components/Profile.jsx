e241f546c3ea8aaf2735908139d1b3288a403162
Vladislav Pakhomov <vladislav_pakhomov@epam.com>
2022-05-10 20:48:11 +0300

    ! Made the basis for the page

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://2code.info/demo/themes/Himer/main/wp-content/uploads/sites/2/2022/03/slider-1.png'></img>
+        <div className='ava'>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+                <div>1 post</div>
+                <div>2 post</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
